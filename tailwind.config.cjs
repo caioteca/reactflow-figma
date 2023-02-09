68778c9b4b5080207c6b6ab33db65f05ab38730d
caioteca <caiodev2020@gmail.com>
2023-02-09 09:59:25 +0100

    Ola

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.tsx',
+    './public/**/*.html',
+    './public/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
